47d748a46fd3f5f2892e6c860865b4668198f4e4
zhongerakeman <1960591723@qq.com>
2023-07-11 16:28:03 +0800

    demo

--- JavaProject/src/cn/clem/HelloWorld.java
+++ JavaProject/src/cn/clem/HelloWorld.java
@@ -3,7 +3,7 @@ package cn.clem;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        int i = 10;
+        int i = 20;
         System.out.println("HelloWorld!");
     }
 
